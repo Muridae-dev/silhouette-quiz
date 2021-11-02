a00ce9fea988c480574269d475626032a86b63ad
Anna Sakurai <zenasak@gmail.com>
2021-11-02 10:09:36 +0100

    add img, img-card.  Randomize questions. Match correct answer.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  deleteDoc,
+  getDocs,
+} from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
+
+//Add your own config content
+const firebaseConfig = {
+  apiKey: 'AIzaSyARqljuS5yf86OHAp8H1u9q0j6I6m1Scss',
+  authDomain: 'quiz-01-2c676.firebaseapp.com',
+  projectId: 'quiz-01-2c676',
+  storageBucket: 'quiz-01-2c676.appspot.com',
+  messagingSenderId: '180059847819',
+  appId: '1:180059847819:web:768ec49d4e155787f9688e',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const imageCards = document.getElementById('cards');
 const question = document.getElementById('question');
+
+let score = 0;
 
 const silhouetteImg = [
   {
     id: '01',
-    src: './images/Luke.png',
+    source: './images/Luke.png',
   },
   {
     id: '02',
-    src: './images/Ichigo.png',
+    source: './images/Ichigo.png',
   },
   {
     id: '03',
-    src: './images/Naruto.png',
+    source: './images/Naruto.png',
   },
   {
     id: '04',
-    src: './images/Yukimaru.png',
+    source: './images/Yukimaru.png',
   },
 ];
 
@@ -30,26 +55,38 @@ const questionAndAnswer = [
     correctId: '02',
   },
   {
-    question: 'Who has a name which a type of topping you put in ramen',
+    question: 'Who has a name which a type of topping you put in ramen?',
     correctId: '03',
   },
 ];
 
+function random(array) {
+  let index = Math.floor(Math.random() * array.length);
+  return index;
+}
+
+let randomIndex = random(questionAndAnswer);
+console.log(randomIndex);
+
+question.innerHTML = questionAndAnswer[randomIndex].question;
+
 function createCards(img) {
-  for (let i = 0; i < 5; i++) {
-    img.innerHTML += `<img class="imgBtn" src="${silhouetteImg[i].src}" />`;
+  for (let i = 0; i < silhouetteImg.length; i++) {
+    img.innerHTML += `<img class="imgBtn" id="0${i + 1}" src="${
+      silhouetteImg[i].source
+    }" onclick="onClick(this.id)" />`;
   }
 }
 
 createCards(imageCards);
 
-question.innerHTML =
-  questionAndAnswer[
-    Math.floor(Math.random() * questionAndAnswer.length)
-  ].question;
-console.log(question);
-
 //add EventListener for clicking a card
+function onClick(clicked_id) {
+  if (clicked_id === questionAndAnswer[randomIndex].correctId) {
+    console.log(clicked_id);
+    score++;
+  }
+}
 
 //compare if the clicked item is correct answer
 
